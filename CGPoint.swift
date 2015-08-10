7a43aa11641642f0e51a3cdc919f0be4778257ad
Mattias Eriksson <thematerik@gmail.com>
2015-08-10 16:35:17 +0200

    ADDED more vector logic

--- CGPoint.swift
+++ CGPoint.swift
@@ -8,6 +8,31 @@
 
 import UIKit
 
-func +(pnt: CGPoint, size: CGSize) -> CGPoint {
+public extension CGPoint {
+    
+    init(squared: CGFloat) {
+        self.init(x: squared, y: squared)
+    }
+    
+    func exists(inFrame frame: CGRect) -> Bool {
+        return (self.x >= frame.origin.x && self.x <= (frame.origin.x + frame.size.width) &&
+            self.y >= frame.origin.y && self.y <= (frame.origin.y + frame.size.height))
+    }
+    
+}
+
+public func +(pnt1: CGPoint, pnt2: CGPoint) -> CGPoint {
+    return CGPoint(x: pnt1.x + pnt2.x, y: pnt1.y + pnt2.y)
+}
+
+public func -(pnt1: CGPoint, pnt2: CGPoint) -> CGPoint {
+    return CGPoint(x: pnt1.x - pnt2.x, y: pnt1.y - pnt2.y)
+}
+
+public func +(pnt: CGPoint, size: CGSize) -> CGPoint {
     return CGPoint(x: pnt.x + size.width, y: pnt.y * size.height)
+}
+
+public func -(ptn: CGPoint, size: CGSize) -> CGPoint {
+    return CGPoint(x: ptn.x - size.width, y: ptn.y - size.height)
 }
--- CGSize.swift
+++ CGSize.swift
@@ -8,6 +8,22 @@
 
 import UIKit
 
-func *(scalar: CGFloat, size: CGSize) -> CGSize {
+public extension CGSize {
+    
+    init(squared: CGFloat) {
+        self.init(width: squared, height: squared)
+    }
+    
+}
+
+public func *(scalar: CGFloat, size: CGSize) -> CGSize {
     return CGSize(width: scalar * size.width, height: scalar * size.height)
+}
+
+public func +(size1: CGSize, size2: CGSize) -> CGSize {
+    return CGSize(width: size1.width + size2.width, height: size1.height + size2.height)
+}
+
+public func -(size1: CGSize, size2: CGSize) -> CGSize {
+    return CGSize(width: size1.width - size2.width, height: size1.height - size2.height)
 }
