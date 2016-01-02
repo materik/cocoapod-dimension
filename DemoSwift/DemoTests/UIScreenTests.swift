ff3a7869a2e1499a79e9481ebcbcebcfaa461f1b
Mattias Eriksson <thematerik@gmail.com>
2016-01-02 03:13:59 +0100

    [podspec] bump version to 1.2

--- DemoSwift/DemoTests/UIScreenTests.swift
+++ DemoSwift/DemoTests/UIScreenTests.swift
@@ -16,17 +16,17 @@ class UIScreenTests: XCTestCase {
     
     func testGetWidth() {
         let mainScreen = UIScreen.mainScreen()
-        XCTAssertEqual(mainScreen.bounds.width, mainScreen.width)
+        XCTAssertEqual(mainScreen.bounds.size.width, mainScreen.width)
         XCTAssertEqual(mainScreen.width, 320)
-        XCTAssertEqual(mainScreen.bounds.width, UIScreen.width)
+        XCTAssertEqual(mainScreen.bounds.size.width, UIScreen.width)
         XCTAssertEqual(UIScreen.width, 320)
     }
     
     func testGetW() {
         let mainScreen = UIScreen.mainScreen()
-        XCTAssertEqual(mainScreen.bounds.width, mainScreen.w)
+        XCTAssertEqual(mainScreen.bounds.size.width, mainScreen.w)
         XCTAssertEqual(mainScreen.w, 320)
-        XCTAssertEqual(mainScreen.bounds.width, UIScreen.w)
+        XCTAssertEqual(mainScreen.bounds.size.width, UIScreen.w)
         XCTAssertEqual(UIScreen.w, 320)
     }
 
@@ -34,17 +34,17 @@ class UIScreenTests: XCTestCase {
     
     func testGetHeight() {
         let mainScreen = UIScreen.mainScreen()
-        XCTAssertEqual(mainScreen.bounds.height, mainScreen.height)
+        XCTAssertEqual(mainScreen.bounds.size.height, mainScreen.height)
         XCTAssertEqual(mainScreen.height, 480)
-        XCTAssertEqual(mainScreen.bounds.height, UIScreen.height)
+        XCTAssertEqual(mainScreen.bounds.size.height, UIScreen.height)
         XCTAssertEqual(UIScreen.height, 480)
     }
     
     func testGetH() {
         let mainScreen = UIScreen.mainScreen()
-        XCTAssertEqual(mainScreen.bounds.height, mainScreen.h)
+        XCTAssertEqual(mainScreen.bounds.size.height, mainScreen.h)
         XCTAssertEqual(mainScreen.h, 480)
-        XCTAssertEqual(mainScreen.bounds.height, UIScreen.h)
+        XCTAssertEqual(mainScreen.bounds.size.height, UIScreen.h)
         XCTAssertEqual(UIScreen.h, 480)
     }
 
